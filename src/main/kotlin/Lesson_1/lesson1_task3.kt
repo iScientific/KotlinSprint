56f65487b7da9003d4dd371f1e7b3c2d71e3def2
Aleksandr <scienntific@yandex.ru>
2025-03-30 15:45:38 +0500

    KS-1-3 add boarding time

--- src/main/kotlin/Lesson_1/lesson1_task3.kt
+++ src/main/kotlin/Lesson_1/lesson1_task3.kt
@@ -4,8 +4,16 @@ fun main() {
     val year: Short = 1961
     var hour: Byte = 9
     var minute: Byte = 7
+    println("Время взлета")
     println(year)
     println(hour)
+    println(minute)
+
+    hour = 10
+    minute = 55
+    println("Время посадки")
+    print(hour)
+    print(":")
     println(minute)
 
 }

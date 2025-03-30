db919fe5f707ed2cf25272b962922a04e6cb0492
Aleksandr <scienntific@yandex.ru>
2025-03-30 17:29:50 +0500

    KS-2-1 calculate and print the average value

---
+++ src/main/kotlin/Lesson_2/lesson2_task1.kt
@@ -0,0 +1,13 @@
+package org.example.lesson2
+
+fun main() {
+    val student1: Int = 3
+    val student2: Int = 4
+    val student3: Int = 3
+    val student4: Int = 5
+    var averageVal: Float = 0.0F
+        averageVal = (averageVal + student1 + student2 + student3 + student4) / 4
+    println(averageVal)
+
+}
+

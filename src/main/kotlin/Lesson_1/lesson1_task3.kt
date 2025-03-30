bb092a83caff2a4bdc474e29529e72c11972a4cf
Aleksandr <scienntific@yandex.ru>
2025-03-30 15:40:11 +0500

    KS-1-3 create time takeoff

---
+++ src/main/kotlin/Lesson_1/lesson1_task3.kt
@@ -0,0 +1,11 @@
+package org.example.Lesson_1
+
+fun main() {
+    val year: Short = 1961
+    var hour: Byte = 9
+    var minute: Byte = 7
+    println(year)
+    println(hour)
+    println(minute)
+
+}

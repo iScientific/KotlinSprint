a7b2e9d10cc83429c54ae7cacb431d8180e1ca8e
Aleksandr <scienntific@yandex.ru>
2025-03-30 11:43:50 +0500

    KS-1-1 add VARIABLES

--- src/main/kotlin/Lesson_1/lesson1_task1.kt
+++ src/main/kotlin/Lesson_1/lesson1_task1.kt
@@ -1,2 +1,11 @@
 package org.example.Lesson_1
 
+fun main() {
+
+    var namOfOrder: Int = 75
+    val thanksBuy: String = "Спасибо за покупку. Ждем вас в нашем магазине снова!"
+
+    print("КОЛИЧЕСТВО ЗАКАЗОВ: ")
+    println(namOfOrder)
+    println(thanksBuy)
+}

4460b9acbdbb5968f99662dbedaea646211e7b88
Aleksandr <scienntific@yandex.ru>
2025-04-03 11:31:51 +0500

    KS-2-4 fixed percent

--- src/main/kotlin/Lesson_2/lesson2_task4.kt
+++ src/main/kotlin/Lesson_2/lesson2_task4.kt
@@ -3,7 +3,8 @@ package org.example.Lesson_2
 fun main() {
     val crystalOre: Byte = 7
     val ironOre: Byte = 11
+    val percent: Byte = 20
 
-    println(crystalOre / 5)
-    println(ironOre / 5)
+    println(crystalOre * percent / 100)
+    println(ironOre * percent / 100)
 }

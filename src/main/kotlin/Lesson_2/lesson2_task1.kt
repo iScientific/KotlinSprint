bf78a8d9f2f1d62113fda6964409b2641a5345f6
Aleksandr <scienntific@yandex.ru>
2025-04-03 11:21:16 +0500

    KS-2-1 fixed numbers of students

--- src/main/kotlin/Lesson_2/lesson2_task1.kt
+++ src/main/kotlin/Lesson_2/lesson2_task1.kt
@@ -1,12 +1,13 @@
 package org.example.lesson2
 
 fun main() {
-    val student1: Int = 3
-    val student2: Int = 4
-    val student3: Int = 3
-    val student4: Int = 5
+    val student1: Byte = 3
+    val student2: Byte = 4
+    val student3: Byte = 3
+    val student4: Byte = 5
+    var numOfStudents: Byte = 4
     var averageVal: Float = 0.0F
-        averageVal = (averageVal + student1 + student2 + student3 + student4) / 4
+    averageVal = (averageVal + student1 + student2 + student3 + student4) / numOfStudents
     println(averageVal)
 
 }

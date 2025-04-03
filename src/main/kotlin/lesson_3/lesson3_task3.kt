05b0b157281a875d75ab2ba0605973cff6c4ea6b
Aleksandr <scienntific@yandex.ru>
2025-04-03 20:07:02 +0500

    KS-3-3 multiplication table

---
+++ src/main/kotlin/lesson_3/lesson3_task3.kt
@@ -0,0 +1,19 @@
+package org.example.lesson_3
+
+fun main() {
+    val valueTabular: Byte = 8
+
+    println(
+        """
+        $valueTabular * 1 = ${valueTabular * 1}
+        $valueTabular * 2 = ${valueTabular * 2}
+        $valueTabular * 3 = ${valueTabular * 3}
+        $valueTabular * 4 = ${valueTabular * 4}
+        $valueTabular * 5 = ${valueTabular * 5}
+        $valueTabular * 6 = ${valueTabular * 6}
+        $valueTabular * 7 = ${valueTabular * 7}
+        $valueTabular * 8 = ${valueTabular * 8}
+        $valueTabular * 9 = ${valueTabular * 9}
+    """.trimMargin()
+    )
+}

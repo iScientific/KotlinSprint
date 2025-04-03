2714949f58ba6e2cf5a05346dfa93b6fe21d7065
Aleksandr <scienntific@yandex.ru>
2025-04-03 19:55:01 +0500

    KS-3-2 marriage

---
+++ src/main/kotlin/lesson_3/lesson3_task2.kt
@@ -0,0 +1,16 @@
+package org.example.lesson_3
+
+fun main() {
+    val nameUser: String = "Tatyana"
+    val patronymic: String = "Sergeevna"
+    var surname: String = "Andreeva"
+    var ageUser: String = "20"
+
+    println("$surname $nameUser $patronymic, $ageUser")
+
+    surname = "Sidorova"
+    ageUser = "22"
+
+    println("$surname $nameUser $patronymic, $ageUser")
+
+}
